eff77a8e6aa83a03cdf71ee980557cdf960cac75
NguyenMinhQuan <nguyenminhquan341@gmail.com>
2025-10-27 18:08:45 +0700

    Update product card UI and adjust grid spacing
    Refined the ProductCard component with improved layout, updated styles, and added a cart icon to the 'Add to cart' button. Also increased the grid gap in ProductsList for better spacing between product cards.

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,18 +1,20 @@
 function ProductCard({ product }: { product: Product }) {
     return (
-        <div className="border p-4 rounded-lg shadow-md">
+        <div className="pb-2">
             <div className="relative">
                 <img
                     src={product.image.thumbnail}
                     alt={product.name}
-                    className="w-full rounded-2xl mb-4"
+                    className="w-full rounded-2xl mb-6"
                 />
-                <div className="absolute -bottom-5 rounded-full bg-rose-50 mx-8 p-2 inset-x-0 text-center cursor-pointer">Add to cart</div>
+                <div className="absolute -bottom-5 rounded-full bg-rose-50 mx-8 p-2 inset-x-0 cursor-pointer border-rose-400 border flex justify-center gap-2">
+                    <img src="assets/images/icon-add-to-cart.svg" alt="cart-icon" />
+                    Add to cart
+                </div>
             </div>
-            <h3 className="text-xl font-semibold mt-2">{product.name}</h3>
-            <p>Category: {product.category}</p>
-            <p>Price: ${product.price}</p>
-            <button>Add to Cart</button>
+            <span className="text-rose-400 font-semibold text-sm">{product.category}</span>
+            <h3 className="text-md font-semibold mb-2">{product.name}</h3>   
+            <span className="font-semibold text-red-custom">${product.price.toFixed(2)}</span>
         </div>
     );
 }
--- app/components/ProductsList.tsx
+++ app/components/ProductsList.tsx
@@ -5,7 +5,7 @@ function ProductsList() {
     return (
         <div className="w-2/3 p-2 h-full">
             <h1 className="text-4xl font-bold my-5">Desserts</h1>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
                 {data.map(product => (
                     <ProductCard key={product.name} product={product} />
                 ))}
